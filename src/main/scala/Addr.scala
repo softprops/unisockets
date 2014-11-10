4819c380eeab9fad395d5972a29687c519b4d5a4
softprops <d.tangren@gmail.com>
2014-11-09 19:10:26 -0500

    when possible, expose the file path as the hostname

--- src/main/scala/Addr.scala
+++ src/main/scala/Addr.scala
@@ -5,9 +5,11 @@ import java.io.File
 import jnr.unixsocket.UnixSocketAddress
 
 object Addr {
-  def apply(file: File): Addr = Addr(new UnixSocketAddress(file))
+  def apply(file: File): Addr = Addr(new UnixSocketAddress(file), Some(file))
 }
 
-case class Addr(addr: UnixSocketAddress) extends InetSocketAddress(0) {
+case class Addr
+ (addr: UnixSocketAddress, file: Option[File] = None)
+  extends InetSocketAddress(file.map(_.getAbsolutePath).orNull, 0) {
   override def toString() = s"Addr($addr)"
 }
