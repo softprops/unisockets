957c18a08fcf2898cef07a2093fa7477fbb8c3a0
softprops <d.tangren@gmail.com>
2014-12-04 00:07:19 -0500

    test a few other http methods

--- unisockets-netty/src/test/scala/ClientUdsSocketChannelFactorySpec.scala
+++ unisockets-netty/src/test/scala/ClientUdsSocketChannelFactorySpec.scala
@@ -24,10 +24,18 @@ class ClientSocketChannelFactorySpec extends FunSpec {
           sockets
         )
       ))
-      val future = http((Req(identity)
+      val docker = (Req(identity)
              .setVirtualHost("unix:///var/run/docker.sock")
-             .setProxyServer(new ProxyServer("unix:///var/run/docker.sock", 80))) / "images" / "json" > as.String)
-      println(Await.result(future, 3.seconds))
+             .setProxyServer(new ProxyServer("unix:///var/run/docker.sock", 80)))
+
+      val images = http(docker / "images" / "json" > as.String)
+      println(Await.result(images, 3.seconds))
+
+      val create = http(docker.POST / "containers" / "create" << """{"Image":"redis:latest"}""" > as.String)
+      println(Await.result(create, 3.seconds))
+
+      val containers = http(docker / "containers" / "json" > as.String)
+      println(Await.result(containers, 3.seconds))
       // ahc won't call this on close - https://github.com/AsyncHttpClient/async-http-client/blob/async-http-client-1.8.14/src/main/java/com/ning/http/client/providers/netty/NettyAsyncHttpProvider.java#L912
       sockets.releaseExternalResources()
       http.shutdown()
