c1015415f2d67a7af40e02ba02dd1e0bf2c35de1
softprops <d.tangren@gmail.com>
2015-01-03 17:09:37 -0500

    Revert "changes needed to upgrade to netty 3.10.0"
    This reverts commit 9d2fe0c8cdee786e7e2dc6e625d90e4d354675cc.

--- unisockets-netty/build.sbt
+++ unisockets-netty/build.sbt
@@ -1,5 +1,5 @@
 libraryDependencies ++= Seq(
-  "io.netty" % "netty" % "3.10.0.Final",
+  "io.netty" % "netty" % "3.9.2.Final",
   "org.scalatest" %% "scalatest" % "2.2.1" % "test",
   "net.databinder.dispatch" %% "dispatch-core" % "0.11.2" % "test",
   "org.slf4j" % "slf4j-log4j12" % "1.6.2" % "test")
--- unisockets-netty/src/main/scala/org/jboss/netty/channel/socket/nio/ClientUdsChannelFactory.scala
+++ unisockets-netty/src/main/scala/org/jboss/netty/channel/socket/nio/ClientUdsChannelFactory.scala
@@ -43,7 +43,7 @@ class ClientUdsSocketChannelFactory
   private[this] lazy val workers: NioWorkerPool =
     new NioWorkerPool(workerExec, DefaultIOThreads) {
 
-      override protected def newWorker(executor: Executor): NioWorker =
+      override protected def createWorker(executor: Executor): NioWorker =
         new NioWorker(executor, null/*threadNameDeterminer*/) {
 
           private[this] val recvPool = new SocketReceiveBufferAllocator()
@@ -158,11 +158,11 @@ class ClientUdsSocketChannelFactory
                         log.debug(s"worker: it's unix")
                         unix.chan.configureBlocking(false)
                         unix.chan.register(
-                          selector, chan.getInternalInterestOps(), chan)
+                          selector, chan.getRawInterestOps(), chan)
                       case other =>
                         log.error(s"worker: it's not unix")
                         other.register(
-                          selector, chan.getInternalInterestOps(), chan)
+                          selector, chan.getRawInterestOps(), chan)
                     }
                     
                     log.debug("worker: registered!")
